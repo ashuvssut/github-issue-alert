0bae9946bf2b5cbf1d968142c5d627d0e0c0f38d
Ashutosh Khanduala <ashu.khanduala@gmail.com>
2025-05-02 04:37:40 +0530

    scss support + add MUI

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,11 +1,18 @@
 import { createRoot } from "react-dom/client";
-import { startIssueChecking } from "./issueFetcher";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Button from "@mui/material/Button";
+
+import "./index.scss";
 
 const App = () => {
-  return <h2>Hello from React!</h2>;
+  return <ButtonUsage />;
 };
 
+export default function ButtonUsage() {
+  return <Button variant="contained">Hello world</Button>;
+}
 const root = createRoot(document.body);
 root.render(<App />);
-
-startIssueChecking();
